57eb078e526fb4c1244144903ac5f6eb2086cb9f
macbook <abc@Raghavs-MacBook-Pro.local>
2022-05-02 19:47:32 +0530

    this

--- src/config/unstoppableDomain.ts
+++ src/config/unstoppableDomain.ts
@@ -1,12 +1,12 @@
-
 import UAuth from '@uauth/js';
 
-const ProductionCredentials = {
+
+const testingCredentials = {
   clientID: '3uxE3Ap3b26HJnPmz0H0hhV7I2pnlk8aQmh5vWjbnOE=',
   clientSecret: 'QfTAU9rmrGxyAtOyuWWVyulqNLKoS4sFzi1EuU9/7Co=',
   scope: 'openid email wallet',
   redirectUri: 'https://candid-selkie-77e78e.netlify.app/callback',
-  postLogoutRedirectUri: 'https://candid-selkie-77e78e9.netlify.app/',
+  postLogoutRedirectUri: 'https://candid-selkie-77e78e.netlify.app/',
 };
 
-export const uauth = new UAuth(ProductionCredentials);
+export const uauth = new UAuth(testingCredentials);
