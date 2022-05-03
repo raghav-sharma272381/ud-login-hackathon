98862c7ebd5ea2a398f0f58e548990135d3b0617
macbook <abc@Raghavs-MacBook-Pro.local>
2022-05-03 18:54:32 +0530

    commmit

--- src/config/unstoppableDomain.ts
+++ src/config/unstoppableDomain.ts
@@ -1,12 +1,11 @@
 import UAuth from '@uauth/js';
 
-
 const ProductionCredentials = {
   clientID: '3uxE3Ap3b26HJnPmz0H0hhV7I2pnlk8aQmh5vWjbnOE=',
   clientSecret: 'QfTAU9rmrGxyAtOyuWWVyulqNLKoS4sFzi1EuU9/7Co=',
   scope: 'openid email wallet',
-  redirectUri: 'https://candid-selkie-77e78e.netlify.app/callback',
-  postLogoutRedirectUri: 'https://candid-selkie-77e78e.netlify.app/',
+  redirectUri: 'https://nifty-sammet-0d2669.netlify.app/callback',
+  postLogoutRedirectUri: 'https://nifty-sammet-0d2669.netlify.app/',
 };
 
 export const uauth = new UAuth(ProductionCredentials);
